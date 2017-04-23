424cf0eaf6a1cc1f1dfe24dc20189d72bc9b8f74
Paul Masurel <paul.masurel@gmail.com>
2017-04-23 16:22:28 +0800

    Bumped version of tantivy to 0.3.1

--- src/commands/merge.rs
+++ src/commands/merge.rs
@@ -1,6 +1,6 @@
 extern crate tantivy;
 
-use tantivy::{Index, SegmentMeta};
+use tantivy::Index;
 use std::path::PathBuf;
 use clap::ArgMatches;
 use futures::Future;
@@ -14,18 +14,25 @@ fn error_msg(err: tantivy::Error) -> String {
 
 pub fn run_merge_cli(argmatch: &ArgMatches) -> Result<(), String> {
     let index_directory = PathBuf::from(argmatch.value_of("index").unwrap());
-    let segment_meta = run_merge(index_directory).map_err(error_msg)?;
-    println!("Merge finished with segment meta {:?}", segment_meta);
-    Ok(())
+    run_merge(index_directory).map_err(error_msg)
+    
+    // we rollback to force a gc.
+
 }
 
 
-fn run_merge(path: PathBuf) -> tantivy::Result<SegmentMeta> {
+fn run_merge(path: PathBuf) -> tantivy::Result<()> {
     let index = Index::open(&path)?;
     let segments = index.searchable_segment_ids()?;
-    index
+    let segment_meta = index
         .writer(HEAP_SIZE)?
         .merge(&segments)
         .wait()
-        .map_err(|_| tantivy::Error::ErrorInThread(String::from("Merge got cancelled")))
+        .map_err(|_| tantivy::Error::ErrorInThread(String::from("Merge got cancelled")));
+    println!("Merge finished with segment meta {:?}", segment_meta);
+    println!("Garbage collect irrelevant segments.");
+    Index::open(&path)?
+        .writer_with_num_threads(1, 40_000_000)?
+        .garbage_collect_files()?;
+    Ok(())
 }
