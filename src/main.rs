46f4af07a8c00d605f3795505729e6ae77b697d8
Ewan Higgs <ewan_higgs@yahoo.co.uk>
2018-03-08 23:01:38 +0100

    Prevent panics when bad state is found - e.g. no meta.json or no indexing dir.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ extern crate byteorder;
 #[macro_use]
 extern crate serde_derive;
 
-
 use clap::{AppSettings, Arg, App, SubCommand};
 mod commands;
 use self::commands::*;
@@ -137,5 +136,12 @@ fn main() {
         "bench" => run_bench_cli,
         _ => panic!("Subcommand {} is unknown", subcommand)
     };
-    run_cli(options).unwrap();
+
+    if let Err(ref e) = run_cli(options) {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing ot stderr";
+        writeln!(stderr, "{}", e).expect(errmsg);
+        ::std::process::exit(1);
+    }
 }
